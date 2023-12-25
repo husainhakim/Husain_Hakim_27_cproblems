a67cb41da8dc7fbece59b4fe3922d8ad7f53020d
husainhakim <142593070+husainhakim@users.noreply.github.com>
2023-12-26 00:04:28 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+   
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+
+    printf("\nBefore swapping:\n");
+    printf("a = %d, b = %d\n", a, b);
+
+    
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("\nAfter swapping:\n");
+    printf("a = %d, b = %d\n", a, b);
+
+    return 0;
+}
